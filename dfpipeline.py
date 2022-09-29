414413d85dea7431b179eef71604eef9322af610
lokinegalur <negalurlohith@gmail.com>
2022-09-29 13:45:19 +0000

    added pipeline

--- dfpipeline.py
+++ dfpipeline.py
@@ -10,17 +10,23 @@ from apache_beam.options.pipeline_options import SetupOptions
 from apache_beam.options.pipeline_options import GoogleCloudOptions
 from apache_beam.options.pipeline_options import StandardOptions
 import requests
+#from tenacity import *
 
 class CustomParam(PipelineOptions):
     @classmethod
     def _add_argparse_args(cls, parser):
         parser.add_value_provider_argument('--url1', dest='url1', required=False, help='apiURL')
         parser.add_value_provider_argument('--gcsBucket', dest='gcsBucket', required=False, help='Bucket name')
+        parser.add_value_provider_argument('--dataset', dest='dataset', required=False, help='bigquery dataset')
+        parser.add_value_provider_argument('--tableID', dest='tableID', required=False, help='bigquery table name')
+        #parser.add_value_provider_argument('--authKey', dest='authKey', required=False, help='api auth key')
         parser.add_value_provider_argument('--secretManagerID', dest='secretManagerID', required=False, help='api auth key')
 
 
+#@retry(stop=stop_after_attempt(5), wait=wait_fixed(3))
 class api_data(beam.DoFn) :
   def __init__(self):
+    # TODO(BEAM-6158): Revert the workaround once we can pickle super() on py3.
     # super(WordExtractingDoFn, self).__init__()
     beam.DoFn.__init__(self)
 
@@ -31,7 +37,7 @@ class api_data(beam.DoFn) :
         logging.info("Fetching data from public api url:{}".format(url))
         logging.info("fetching from api")
         response_API = requests.get(url)
-        #converting response to json
+        #converting it to json object
         data = response_API.json()
         logging.info("fetched data:{}".format(data))
         return [data]
@@ -42,26 +48,65 @@ class api_data(beam.DoFn) :
 
 class upload_blob(beam.DoFn) :
   def __init__(self):
+    # TODO(BEAM-6158): Revert the workaround once we can pickle super() on py3.
     # super(WordExtractingDoFn, self).__init__()
     beam.DoFn.__init__(self)
 
   def process(self, element):
+        try:
           """
           element:json object which we fetch from public api
-          Uploads a json file to the bucket."""
+          Uploads a json file to the bucket"""
           logging.info("json file:{}".format(element))
           bucket_name=str(custom_options.gcsBucket.get())
           print("bucket name:{}".format(bucket_name))
           destination_blob_name="external_api_data/ingested_data_{}.json".format(int(time.time()))
-          logging.info("data uploadation start at bucket path is:gs://{}/{}".format(bucket_name,destination_blob_name))
+          logging.info("data uploading started at bucket path :gs://{}/{}".format(bucket_name,destination_blob_name))
           storage_client = storage.Client()
           bucket = storage_client.get_bucket(bucket_name)
           blob = bucket.blob(destination_blob_name)
-          blob.upload_from_string(json.dumps(element))
 
+          blob.upload_from_string(json.dumps(element))
+          gcs_path="gs://{}/{}".format(bucket_name,destination_blob_name)
+          return [gcs_path]
+        except Exception as e:
+            logging.error("Failed to upload data inside bucket:{}".format(e))
+
+
+class gcs_to_bq(beam.DoFn) :
+    def __init__(self):
+        # TODO(BEAM-6158): Revert the workaround once we can pickle super() on py3.
+        # super(WordExtractingDoFn, self).__init__()
+        beam.DoFn.__init__(self)
+
+    def process(self, element):
+        try:
+            """element:gcs json file path
+               Uploading data from gcs to bigquery without auto schema"""
+
+            projectID = "q-gcp-8566-nj-dhs-22-08"
+            # Construct a BigQuery client object.
+            client = bigquery.Client()
+            dataset=str(custom_options.dataset.get())
+            tableName=str(custom_options.tableID.get())
+            table_id="{}.{}.{}".format(projectID,dataset,tableName)
+            print(table_id)
+
+            job_config = bigquery.LoadJobConfig(
+                autodetect=True, source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+            )
+            load_job = client.load_table_from_uri(
+                element, table_id, job_config=job_config
+            )
+            load_job.result()
+            destination_table = client.get_table(table_id)
+            print("Loaded {} rows.".format(destination_table.num_rows))
+        except Exception as e:
+            logging.error("failed to ingest data in bigquery error is:{}".format(e))
 
 def run(argv=None):
     global custom_options
+    projectID="q-gcp-8566-nj-dhs-22-08"
     pipeline_options = PipelineOptions()
     custom_options = pipeline_options.view_as(CustomParam)
     google_cloud_options = pipeline_options.view_as(GoogleCloudOptions)
@@ -81,8 +126,9 @@ def run(argv=None):
     results = (
         p
         |"Create Pipeline" >> beam.Create(["Start"])
-        | 'Fetch data from public api' >> beam.ParDo(api_data())
+        | 'fetch data from api' >> beam.ParDo(api_data())
         | 'upload data to bucket ' >> beam.ParDo(upload_blob())
+        | 'Write to BigQuery' >>beam.ParDo(gcs_to_bq())
         | beam.Map(print))
 
     res=p.run()
